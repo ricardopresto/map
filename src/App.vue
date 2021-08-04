1408dfdd1b9585feb991ae7c15443eff020ed185
ricardopresto <ricardopresto5@gmail.com>
2021-08-04 12:55:10 +0200

    basic marker added

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <div id="app">
+  <div id="app" @click="createMarker">
+    <AddIcon />
     <div id="map"></div>
   </div>
 </template>
 
 <script>
-import mapboxgl from "mapbox-gl";
+import mapboxgl from 'mapbox-gl';
+import AddIcon from './components/AddIcon.vue';
 
 export default {
   name: 'App',
+  components: {
+    AddIcon
+  },
   data() {
     return {
       access_token: 'pk.eyJ1IjoicmljYXJkb3ByZXN0byIsImEiOiJja3J1bmloMHEwYnY4MnBzNWt6b2x6eHBuIn0.zivT6cbQcLcAFl1Q8GW-QA',
@@ -16,19 +21,27 @@ export default {
       map: {},
     };
   },
+
   mounted() {
     this.createMap()
   },
 
   methods: {
     createMap() {
-        mapboxgl.accessToken = this.access_token;
-        this.map = new mapboxgl.Map({
+      mapboxgl.accessToken = this.access_token;
+      this.map = new mapboxgl.Map({
         container: "map",
         style: "mapbox://styles/mapbox/streets-v11",
         center: this.center,
         zoom: 8,
       });
+    },
+    createMarker() {
+      var el = document.createElement('div');
+      el.className = 'marker';
+      var marker = new mapboxgl.Marker({element: el, draggable: true})
+      marker.setLngLat([5.483333, 51.433333])
+      marker.addTo(this.map);
     }
   },
 }
@@ -39,6 +52,11 @@ export default {
 #map {
   width: 90vw;
   height: 90vh;
+  border: 1px solid black;
+}
+.marker{
+  width: 100px;
+  height: 20px;
   border: 1px solid black;
 }
 </style>
