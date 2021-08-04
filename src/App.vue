56d7dcfd46e81db3a728585fcc58e6c9bd067870
ricardopresto <ricardopresto5@gmail.com>
2021-08-04 11:35:27 +0200

    base map added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mapboxgl from "mapbox-gl";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      access_token: 'pk.eyJ1IjoicmljYXJkb3ByZXN0byIsImEiOiJja3J1bmloMHEwYnY4MnBzNWt6b2x6eHBuIn0.zivT6cbQcLcAFl1Q8GW-QA',
+      center: [5.483333, 51.433333],
+      map: {},
+    };
+  },
+  mounted() {
+    this.createMap()
+  },
+
+  methods: {
+    createMap() {
+        mapboxgl.accessToken = this.access_token;
+        this.map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: this.center,
+        zoom: 8,
+      });
+    }
+  },
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#map {
+  width: 90vw;
+  height: 90vh;
+  border: 1px solid black;
 }
 </style>
