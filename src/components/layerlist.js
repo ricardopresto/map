cbf90c9bc67267ef072ec4e1865714a606f1f07a
ricardopresto <ricardopresto5@gmail.com>
2021-08-06 17:55:56 +0200

    added boundaries option

--- src/components/layerlist.js
+++ src/components/layerlist.js
@@ -1,7 +1,8 @@
 const layerlist = [
   {id: 'water', name: 'Water'},
   {id: 'road', name: 'Roads'},
-  {id: 'label', name: 'Labels'}
+  {id: 'label', name: 'Labels'},
+  {id: 'boundary', name: 'Boundaries'}
 ]
 
 export default layerlist;
