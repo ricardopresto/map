0495e0ecbeb992f05c9019649b0507601f9fe662
ricardopresto <ricardopresto5@gmail.com>
2021-08-15 10:32:50 +0200

    code tidying

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,15 @@
         <span>CLOSE</span>
       </div>
     </div>
-    <div id="map" :style="mapStyle" @contextmenu.prevent="removeMarker" @dragover.prevent
-      @dragenter.prevent @drop.prevent="dropMarker"></div>
-      <ControlPanel 
+    <div
+      id="map"
+      :style="mapStyle"
+      @contextmenu.prevent="removeMarker"
+      @dragover.prevent
+      @dragenter.prevent
+      @drop.prevent="dropMarker"
+    ></div>
+    <ControlPanel
       @capture="capture"
       @height-change="heightChange"
       @width-change="widthChange"
@@ -22,21 +28,20 @@
       :windowWidth="windowWidth"
       :windowHeight="windowHeight"
       :markerTextSize="markerTextSize"
-      /> 
+    />
   </div>
 </template>
 
 <script>
-import mapboxgl from 'mapbox-gl';
-import html2canvas from 'html2canvas';
-import { v4 as uuidv4 } from 'uuid';
-import ControlPanel from './components/ControlPanel.vue'
-import iconlist from './components/iconlist';
-import layerlist from './components/layerlist';
+import mapboxgl from "mapbox-gl";
+import html2canvas from "html2canvas";
+import { v4 as uuidv4 } from "uuid";
+import ControlPanel from "./components/ControlPanel.vue";
+import iconlist from "./components/iconlist";
+import layerlist from "./components/layerlist";
 
 export default {
-
-  name: 'App',
+  name: "App",
 
   components: {
     ControlPanel
@@ -44,7 +49,8 @@ export default {
 
   data() {
     return {
-      access_token: 'pk.eyJ1IjoicmljYXJkb3ByZXN0byIsImEiOiJja3J1bmloMHEwYnY4MnBzNWt6b2x6eHBuIn0.zivT6cbQcLcAFl1Q8GW-QA',
+      access_token:
+        "pk.eyJ1IjoicmljYXJkb3ByZXN0byIsImEiOiJja3J1bmloMHEwYnY4MnBzNWt6b2x6eHBuIn0.zivT6cbQcLcAFl1Q8GW-QA",
       center: [5.483333, 51.433333],
       map: {},
       completeLayerList: [],
@@ -56,19 +62,18 @@ export default {
       windowWidth: window.innerWidth,
       windowHeight: window.innerHeight,
       markerTextSize: 14,
-      draggedIconType: '',
+      draggedIconType: "",
       mapWidthOffset: 0,
       mapHeightOffset: 0
-
     };
   },
 
   computed: {
-    mapStyle () {
+    mapStyle() {
       return {
         height: `${this.mapHeight}px`,
         width: `${this.mapWidth}px`
-      }
+      };
     }
   },
 
@@ -82,24 +87,24 @@ export default {
     createMap() {
       mapboxgl.accessToken = this.access_token;
       this.map = new mapboxgl.Map({
-        container: 'map',
-        style: 'mapbox://styles/mapbox/streets-v11',
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
         center: this.center,
         zoom: 8,
         preserveDrawingBuffer: true
       });
-      this.map.on('load', () => {
+      this.map.on("load", () => {
         this.completeLayerList = this.map.getStyle().layers;
         this.map.resize();
-      })
+      });
     },
 
     clickMarker(type) {
       let name = `marker${uuidv4()}`;
-      let icon = document.createElement('img');
+      let icon = document.createElement("img");
       icon.src = this.getIconImage(type);
       icon.id = name;
-      this[name] = new mapboxgl.Marker({element: icon, draggable: true});
+      this[name] = new mapboxgl.Marker({ element: icon, draggable: true });
       this[name].setLngLat(this.adjustCentre());
       this[name].addTo(this.map);
     },
@@ -107,17 +112,23 @@ export default {
     adjustCentre() {
       let locationLngLat = this.map.getCenter();
       let locationXY = this.map.project(locationLngLat);
-      locationXY = {x: locationXY.x - this.mapWidthOffset, y: locationXY.y - this.mapHeightOffset};
+      locationXY = {
+        x: locationXY.x - this.mapWidthOffset,
+        y: locationXY.y - this.mapHeightOffset
+      };
       return this.map.unproject(locationXY);
     },
 
     dropMarker(e) {
       let name = `marker${uuidv4()}`;
-      let icon = document.createElement('img');
-      let dropLocation = {x: e.clientX - this.mapWidthOffset, y: e.clientY - this.mapHeightOffset};
+      let icon = document.createElement("img");
+      let dropLocation = {
+        x: e.clientX - this.mapWidthOffset,
+        y: e.clientY - this.mapHeightOffset
+      };
       icon.src = this.getIconImage(this.draggedIconType);
       icon.id = name;
-      this[name] = new mapboxgl.Marker({element: icon, draggable: true});
+      this[name] = new mapboxgl.Marker({ element: icon, draggable: true });
       this[name].setLngLat(this.map.unproject(dropLocation));
       this[name].addTo(this.map);
     },
@@ -125,18 +136,23 @@ export default {
     addTextMarker(textInfo) {
       this.markerTextSize = textInfo[1];
       let name = `marker${uuidv4()}`;
-      let textMarker = document.createElement('div');
+      let textMarker = document.createElement("div");
       textMarker.innerHTML = textInfo[0];
-      textMarker.style.fontSize = `${this.markerTextSize}pt`
+      textMarker.style.fontSize = `${this.markerTextSize}pt`;
       textMarker.id = name;
-      textMarker.onclick = (e) => {e.preventDefault()};
-      this[name] = new mapboxgl.Marker({element: textMarker, draggable: true});
+      textMarker.onclick = e => {
+        e.preventDefault();
+      };
+      this[name] = new mapboxgl.Marker({
+        element: textMarker,
+        draggable: true
+      });
       this[name].setLngLat(this.adjustCentre());
       this[name].addTo(this.map);
     },
 
     getIconImage(type) {
-      return require(`./assets/icons/${type}`)
+      return require(`./assets/icons/${type}`);
     },
 
     updateDrag(type) {
@@ -145,29 +161,30 @@ export default {
 
     removeMarker(e) {
       let name = e.target.id;
-      if (name.substring(0, 6) == 'marker') {
+      if (name.substring(0, 6) == "marker") {
         this[name].remove();
       }
     },
 
     setVisible(info) {
       for (let item of this.completeLayerList) {
-          if (info[1] == false) {
-            if (item.id.includes(info[0])) {
-              this.map.setLayoutProperty(item.id, 'visibility', 'none');
-            }
-          } else {if (item.id.includes(info[0])) {
-              this.map.setLayoutProperty(item.id, 'visibility', 'visible');
-            }
+        if (info[1] == false) {
+          if (item.id.includes(info[0])) {
+            this.map.setLayoutProperty(item.id, "visibility", "none");
           }
+        } else {
+          if (item.id.includes(info[0])) {
+            this.map.setLayoutProperty(item.id, "visibility", "visible");
+          }
+        }
       }
     },
 
     capture() {
-      let map = document.getElementById('map');
+      let map = document.getElementById("map");
       this.showModal = true;
       html2canvas(map).then(function(canvas) {
-        let modal = document.getElementById('modal');
+        let modal = document.getElementById("modal");
         modal.append(canvas);
       });
     },
@@ -178,26 +195,26 @@ export default {
 
     heightChange(newHeight) {
       this.mapHeight = newHeight;
-      this.mapHeightOffset = document.getElementById('map').offsetTop;
+      this.mapHeightOffset = document.getElementById("map").offsetTop;
     },
 
     widthChange(newWidth) {
       this.mapWidth = newWidth;
-      this.mapWidthOffset = document.getElementById('map').offsetLeft;
+      this.mapWidthOffset = document.getElementById("map").offsetLeft;
     }
   }
-}
+};
 </script>
 
 <style>
 * {
   margin: 0;
-  Padding: 0;
+  padding: 0;
   box-sizing: border-box;
   --switcher-bg: rgba(140, 176, 199, 0.9);
   font-family: Arial, Helvetica, sans-serif;
 }
-#app{
+#app {
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -205,10 +222,10 @@ export default {
   height: 100vh;
   background-color: rgb(175, 206, 224);
 }
-#map{
+#map {
   border: 1px solid white;
 }
-#modal{
+#modal {
   position: absolute;
   display: flex;
   align-items: center;
@@ -218,7 +235,7 @@ export default {
   height: 100vh;
   z-index: 6;
 }
-#closer{
+#closer {
   position: absolute;
   top: 0;
   right: 0;
